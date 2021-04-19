0754be1d822458b9dc0de6e11c0f45efa617bf11
Lucia Garrido <luzia83@gmail.com>
2021-04-19 23:47:15 +0200

    mandatory task done

--- src/stats.ts
+++ src/stats.ts
@@ -1,4 +1,10 @@
-export const stats = [
+
+export interface resultado {
+  name: string;
+  value: number;
+}
+
+export const covidBefore: resultado[] = [
   {
     name: "Madrid",
     value: 174,
@@ -64,3 +70,71 @@ export const stats = [
     value: 6,
   },
 ];
+
+export const covidAfter: resultado[] = [
+  {
+    name: "Madrid",
+    value: 1849,
+  },
+  {
+    name: "La Rioja",
+    value: 97,
+  },
+  {
+    name: "Andalucía",
+    value: 307,
+  },
+  {
+    name: "Cataluña",
+    value: 190,
+  },
+  {
+    name: "Valencia",
+    value: 119,
+  },
+  {
+    name: "Murcia",
+    value: 39,
+  },
+  {
+    name: "Extremadura",
+    value: 85,
+  },
+  {
+    name: "Castilla La Mancha",
+    value: 79,
+  },
+  {
+    name: "País Vasco",
+    value: 727,
+  },
+  {
+    name: "Cantabria",
+    value: 134,
+  },
+  {
+    name: "Asturias",
+    value: 126,
+  },
+  {
+    name: "Galicia",
+    value: 97,
+  },
+  {
+    name: "Aragón",
+    value: 293,
+  },
+  {
+    name: "Castilla y León",
+    value: 387,
+  },
+  {
+    name: "Islas Canarias",
+    value: 271,
+  },
+  {
+    name: "Islas Baleares",
+    value: 40,
+  },
+];
+
